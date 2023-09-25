c74db844b5ad0d716943ca90bc814a9476904d40
Rayumar89 <rayumarj@gmail.com>
2023-09-25 07:22:37 -0700

    second js

---
+++ app.js
@@ -0,0 +1,34 @@
+var myName1 = "umar"
+var myName2 = " farooq"
+
+
+alert(myName1 + myName2);
+
+var num = "15 "
+var name = "years old"
+alert (num + name)
+
+
+
+var name1 = "Certifide Mobile Application Development"
+alert (name1)
+
+
+var name2 = "Pizza\npizz\npiz\npi\np"
+alert (name2)
+
+
+var email1 = "My email address is"
+var email2 = " Rayumarf@gmail.com"
+alert(email1 + email2)
+
+
+
+var book = "I am trying to learn from the Book A smarter way to learn JavaScript"
+alert(book)
+
+
+
+
+var num4 = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+alert (num4)
